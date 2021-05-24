3d4b903db583ef89b7bfb6322ba7724fe4bf7bf7
NateGaNe <nathankong@live.com>
2021-05-24 12:15:23 -0700

    Fixed main

--- main.cpp
+++ main.cpp
@@ -12,15 +12,14 @@
 #include "VisitorLaTex.hpp"
 #include "VistMathML.h"
 int main() {
-    // This is a very basic main, and being able to correctly execute this main
-    // does not constitute a completed lab. Make sure you write unit tests for
-    // all the classes that you create (and can be instantiated) in this lab
     Base* three = new Op(3);
     Base* seven = new Op(7);
     Base* four = new Op(4);
     Base* two = new Op(2);
     Base* mult = new Mult(seven, three);
-std::cout << PrintLaTeX(mult);
-	
+	VisitorLaTeX obj;
+	VisitMathML obj2;
+	std::cout << obj.PrintLaTeX(mult) << std::endl  << obj2.PrintMathML(mult) << std::endl;
+	delete mult;
     return 0;
 }
