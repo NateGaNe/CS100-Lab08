a4a494edfa14d65fce53ad4bdc0e427a27a0b55b
DevoTrix <jchan443@ucr.edu>
2021-05-24 19:02:29 -0700

    finished MathMLTests

--- MathMLTest.hpp
+++ MathMLTest.hpp
@@ -25,5 +25,56 @@ TEST(MathML, regularAdd){
     EXPECT_EQ(test.PrintMathML(add), "<math>\n  <apply>\n    <plus/>\n    <cn>3.000000</cn>\n    <cn>4.000000</cn>\n  </apply>\n</math>");
 }
 
+TEST(MathML, regularSub){
+    Base* fifteen = new Op(15);
+    Base* three = new Op(3);
+    Base* sub = new Sub(fifteen, three);
+    VisitMathML test;
+    EXPECT_EQ(test.PrintMathML(sub),  "<math>\n  <apply>\n    <minus/>\n    <cn>15.000000</cn>\n    <cn>3.000000</cn>\n  </apply>\n</math>" );
+}
+
+TEST(MathML, regularMult){
+    Base* five = new Op(5);
+    Base* three = new Op(3);
+    Base* mult = new Mult(five, three);
+    VisitMathML test;
+    EXPECT_EQ(test.PrintMathML(mult),  "<math>\n  <apply>\n    <times/>\n    <cn>5.000000</cn>\n    <cn>3.000000</cn>\n  </apply>\n</math>");
+}
+
+TEST(MathML, regularPow){
+    Base* eight = new Op(8);
+    Base* two = new Op(2);
+    Base* pow = new Pow(eight, two);
+    VisitMathML test;
+    EXPECT_EQ(test.PrintMathML(pow),  "<math>\n  <apply>\n    <power/>\n    <cn>8.000000</cn>\n    <cn>2.000000</cn>\n  </apply>\n</math>");
+}
+
+TEST(MathML, regDiv){
+    Base* eight = new Op(8);
+    Base* two = new Op(2);
+    Base* div = new Div(eight, two);
+    VisitMathML test;
+    EXPECT_EQ(test.PrintMathML(div),  "<math>\n  <apply>\n    <divide/>\n    <cn>8.000000</cn>\n    <cn>2.000000</cn>\n  </apply>\n</math>");
+}
+
+TEST(MathML, twoFunctions){
+    Base* eight = new Op(8);
+    Base* fifteen = new Op(15);
+    Base* thirteen = new Op(13);
+    Base* minus = new Sub(fifteen, thirteen);
+    Base* div = new Div(eight, minus);
+    VisitMathML test;
+    EXPECT_EQ(test.PrintMathML(div),  "<math>\n  <apply>\n    <plus/>\n    <cn>3.000000</cn>\n    <apply>\n      <minus>\n      <cn>15.000000</cn>\n      <cn>13.000000</cn>\n    </apply>  </apply>\n</math>");
+}
+
+TEST(MathML,moreThanOne){
+    Base* two = new Op(2);
+    Base* five = new Op(5);
+    Base* mult = new Mult(two, five);
+    Base* pow = new Pow(five, two);
+    Base* div = new Div(mult, pow);
+    VisitMathML test;
+    EXPECT_EQ( test.PrintMathML(div), "<math>\n  <apply>\n    <divide/>\n    <apply>\n      <times/>\n      <cn>2</cn>\n      <cn>5</cn>\n    </apply>\n    <apply>\n      <power/>\n      <cn>5</cn>\n      <cn>2</cn>\n    </apply>\n  </apply>\n</math>");
+}
 
 #endif
