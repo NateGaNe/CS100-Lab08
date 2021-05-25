0c71f5419454c8fdda558b3a58dee9a1786c4e0d
DevoTrix <jchan443@ucr.edu>
2021-05-24 19:39:46 -0700

    added deletions

--- MathMLTest.hpp
+++ MathMLTest.hpp
@@ -23,6 +23,7 @@ TEST(MathML, regularAdd){
     Base* add = new Add(three, four);
     VisitMathML test;
     EXPECT_EQ(test.PrintMathML(add), "<math>\n  <apply>\n    <plus/>\n    <cn>3.000000</cn>\n    <cn>4.000000</cn>\n  </apply>\n</math>");
+    delete add;
 }
 
 TEST(MathML, regularSub){
@@ -31,6 +32,7 @@ TEST(MathML, regularSub){
     Base* sub = new Sub(fifteen, three);
     VisitMathML test;
     EXPECT_EQ(test.PrintMathML(sub),  "<math>\n  <apply>\n    <minus/>\n    <cn>15.000000</cn>\n    <cn>3.000000</cn>\n  </apply>\n</math>" );
+    delete sub;
 }
 
 TEST(MathML, regularMult){
@@ -39,6 +41,7 @@ TEST(MathML, regularMult){
     Base* mult = new Mult(five, three);
     VisitMathML test;
     EXPECT_EQ(test.PrintMathML(mult),  "<math>\n  <apply>\n    <times/>\n    <cn>5.000000</cn>\n    <cn>3.000000</cn>\n  </apply>\n</math>");
+    delete mult;
 }
 
 TEST(MathML, regularPow){
@@ -47,6 +50,7 @@ TEST(MathML, regularPow){
     Base* pow = new Pow(eight, two);
     VisitMathML test;
     EXPECT_EQ(test.PrintMathML(pow),  "<math>\n  <apply>\n    <power/>\n    <cn>8.000000</cn>\n    <cn>2.000000</cn>\n  </apply>\n</math>");
+    delete pow;
 }
 
 TEST(MathML, regDiv){
@@ -55,6 +59,7 @@ TEST(MathML, regDiv){
     Base* div = new Div(eight, two);
     VisitMathML test;
     EXPECT_EQ(test.PrintMathML(div),  "<math>\n  <apply>\n    <divide/>\n    <cn>8.000000</cn>\n    <cn>2.000000</cn>\n  </apply>\n</math>");
+    delete div;
 }
 
 TEST(MathML, twoFunctions){
@@ -65,6 +70,7 @@ TEST(MathML, twoFunctions){
     Base* div = new Div(eight, minus);
     VisitMathML test;
     EXPECT_EQ(test.PrintMathML(div),  "<math>\n  <apply>\n    <divide/>\n    <cn>8.000000</cn>\n    <apply>\n      <minus/>\n      <cn>15.000000</cn>\n      <cn>13.000000</cn>\n    </apply>\n  </apply>\n</math>");
+    delete div;
 }
 
 TEST(MathML,moreThanOne){
@@ -75,6 +81,7 @@ TEST(MathML,moreThanOne){
     Base* div = new Div(mult, pow);
     VisitMathML test;
     EXPECT_EQ( test.PrintMathML(div), "<math>\n  <apply>\n    <divide/>\n    <apply>\n      <times/>\n      <cn>2.000000</cn>\n      <cn>5.000000</cn>\n    </apply>\n    <apply>\n      <power/>\n      <cn>5.000000</cn>\n      <cn>2.000000</cn>\n    </apply>\n  </apply>\n</math>");
+    delete div;
 }
 
 #endif
